1a28b30242d45c63da34fe0d405f1081718fb6c7
EvandroRBR <evandro.braga01@fatec.sp.gov.br>
2020-09-29 11:10:48 -0300

    adicionando serviços

--- src/org/fatec/ServiceController.java
+++ src/org/fatec/ServiceController.java
@@ -15,13 +15,18 @@ public class ServiceController {
 		this.services.add(new Services("Manicure", "F"));
 		this.services.add(new Services("Pedicure", "F"));
 		this.services.add(new Services("Corte  e  pintura   de  cabelo", "F"));
+		this.services.add(new Services("Remoção de manchas na pele", "F"));
+		this.services.add(new Services("Aplicação de botox", "F"));
+		this.services.add(new Services("Tratamento para emagrecimento", "F"));
+		this.services.add(new Services("Redução de medidas", "F"));
+		
 		this.services.add(new Services("Corte de cabelo","M"));
 		this.services.add(new Services("Modelagem e corte de barba","M"));
 		this.services.add(new Services("Tratamento para quedas de cabelo","M"));
+		this.services.add(new Services("Produtos especializados","M"));		
 	}
 	
 	public void listServices() {
 		this.services.forEach(service -> System.out.println(service));
 	}
-
 }
